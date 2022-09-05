31ecd948d929a1ead2636970e2bda1c3f53de8d9
Monica Gonzalez calleja <mogonzal@cbr11s1.42barcelona.com>
2022-09-05 18:09:51 +0200

    updated

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,36 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mogonzal <mogonzal@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 16:04:24 by mogonzal          #+#    #+#             */
-/*   Updated: 2022/06/09 19:30:15 by mogonzal         ###   ########.fr       */
+/*   Updated: 2022/06/13 17:04:53 by mogonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 # include <stdio.h>
 # include <unistd.h>
 # include <string.h>
 # include <stdlib.h>
 # include <fcntl.h>
 
-# ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-# endif
-
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
-void	ft_bzero(void *s, size_t n);
 char	*ft_strchr(const char *s, int c);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
-void	*ft_calloc(size_t count, size_t size);
 char	*ft_strjoin(char *s1, char *s2);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*create_malloc_of_strjoin(char *s1);
+char	*ft_substr(char *s, unsigned int start, size_t len);
 char	*ft_get_line(char *stock);
 char	*ft_fill(int fd, char *stock);
 char	*ft_cut_line(char *stock);
